ef8e74447b0dba081489c96f3a7af4caf3899fb4
kdlr <40738758+kathryndelarosa@users.noreply.github.com>
2022-12-20 21:23:58 -0500

    this is just a Form i am realizing

---
+++ centimeters.js
@@ -0,0 +1,10 @@
+// learn what the deal with promptInt() ASAP...
+
+let stGauge = ;
+let bustMeasure = ;
+let trueFit = (bustMeasure / 10) * stGauge;
+
+let ease = ;
+let easeGauge = ease * 10
+
+totalSts = Math.ceil(trueFit + easeGauge);
