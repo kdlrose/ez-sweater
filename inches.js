613946c7bb6dbf0530c8586eb68b3d8e0cf11ed9
kdlr <40738758+kathryndelarosa@users.noreply.github.com>
2022-12-20 21:35:21 -0500

    empty integers

--- inches.js
+++ inches.js
@@ -1,16 +1,10 @@
-let bustMeasure = 47;
-let stGauge = 24;
+// learn what the deal with promptInt() ASAP...
+
+let stGauge = ;
+let bustMeasure = ;
 let trueFit = (bustMeasure / 4) * stGauge;
 
-let ease = 2;
+let ease = ;
 let easeGauge = ease * 4
 
 totalSts = Math.ceil(trueFit + easeGauge);
-
-console.log(`Your total stitch count after you join at the armpits should be ${totalSts} for ${ease} inches of ease.`);
-console.log(`Without ease, this is ${trueFit}.`)
-
-sleeveSts = totalSts *= .2;
-chestSts = totalSts *= .3;
-backBOR = totalSts *= 1.5;
-backEOR = totalSts *= 1.5;
