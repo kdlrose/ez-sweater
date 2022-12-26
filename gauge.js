ef16a53791e3435b8d8154f80ea765927fc71495
kathryn de la rosa <kathryndelarosa@protonmail.com>
2022-12-26 01:33:48 -0500

    made subfolders

--- gauge.js
+++ gauge.js
@@ -1,33 +1,31 @@
 const gauge = 28;
-const bust = 120;
+const bust = 107;
 const ease = 5;
 
 // by cm
 
 finalMeasure = bust + ease;  
 
-neck = Math.round(finalMeasure * .4);
-sleeve = Math.round(finalMeasure * .2);
-underarm = Math.round(finalMeasure * .08);
-cuff = Math.round(finalMeasure * .25);
-
-shoulder = Math.round(neck * .2);
-chest = Math.round(neck * .3);
+neck = finalMeasure * 0.4;
+sleeve = finalMeasure * 0.2;
+underarm = finalMeasure * 0.08;
+cuff = finalMeasure * 0.25;
+shoulder = neck * 0.2;
+chest = neck * 0.3;
 
 // by gauge
-trueSts = 2 * Math.ceil(gauge * (bust / 10) / 2);
-easeSts = 2 * Math.ceil(gauge * (ease / 10) / 2);
 bodySts = trueSts + easeSts;
+neckSts = gauge * (neck / 10);
+sleeveSts = gauge * (sleeve / 10);
+pitSts = gauge * (underarm / 10);
+cuffSts = gauge * (cuff / 10);
 
-neckSts = 2 * Math.ceil(gauge * (neck / 10) / 2);
-sleeveSts = 2 * Math.ceil(gauge * (sleeve / 10) / 2);
-pitSts = 2 * Math.ceil(gauge * (underarm / 10) / 2);
-cuffSts = 2 * Math.ceil(gauge * (cuff / 10) / 2);
-
-shoulderSts = 2 * Math.ceil(gauge * (shoulder / 10) / 2);
-chestSts = 2 * Math.ceil(gauge * (chest / 10) / 2);
+shoulderSts = gauge * (shoulder / 10);
+chestSts = gauge * (chest / 10);
+halfBack = chestSts / 2;
 
 // print
-console.log(`For a ${finalMeasure} cm sweater with 10 cm = ${gauge} sts, the body stitch count should be ${bodySts} sts.
-The neck should measure ${neck} cm at about ${neckSts} sts.
-Each shoulder should start at ${shoulder} cm wide = ${shoulderSts} sts, and increase to at about ${sleeve} cm around = ${sleeveSts} sts.`)
+console.log(`
+For a ${finalMeasure} cm sweater with 10 cm = ${gauge} sts, the body stitch count should be ${bodySts} sts.
+Top down: ${neckSts} neck sts = ${halfBack} sts from middle of back + ${shoulderSts} L shoulder sts + ${chestSts} chest sts + ${shoulderSts} R shoulder sts + ${halfBack} sts.
+`)
